0298a05c3f679d6b12dfbce275b4b3c67a0a04e2
Mihai Galos <mihaigalos@gmail.com>
2018-12-27 17:25:53 +0200

    Improve pylint score to above 9

--- src/tracking.py
+++ src/tracking.py
@@ -1,22 +1,29 @@
+"""
+Simple object tracking implemented in Python 2 using ROS and PyKalman.
+"""
 #!/usr/bin/env python
 
-import rospy
-from geometry_msgs.msg import Pose, Point, Quaternion, Vector3, Polygon
-import rviz_tools_py as rviz_tools
+
 import math
-from tf.transformations import quaternion_from_euler
 from pprint import pprint
-from pykalman import KalmanFilter
-import numpy as np
 import collections  # for deque
+from pykalman import KalmanFilter
+from geometry_msgs.msg import Pose, Point, Quaternion
+import rviz_tools_py as rviz_tools
+import rospy
+from tf.transformations import quaternion_from_euler
 
 
-theta = 0
-radius = 1.5
+class KalmanWrapper(object):
+    """
+    Wrapper for configuring and passing values to and from the Kalman KalmanFilter.
+    """
 
-
-class KalmanWrapper:
-    def __init__(self, initial_state_mean):
+    def __init__(self):
+        """
+        Transition matrix has form [x, x_dot, y, y_dot].
+        Observation matrix has same form.
+        """
         self.transition_matrix = [[1, 1, 0, 0],
                                   [0, 1, 0, 0],
                                   [0, 0, 1, 1],
@@ -25,8 +32,10 @@ class KalmanWrapper:
         self.observation_matrix = [[1, 0, 0, 0],
                                    [0, 0, 1, 0]]
 
-    def get_prediction(self, measurements):
-
+    def make_prediction(self, measurements):
+        """
+        Compute prediction of object position for the next step.
+        """
         initial_state_mean = [measurements[0][0], 0,
                               measurements[0][1], 0]
 
@@ -34,53 +43,85 @@ class KalmanWrapper:
                            observation_matrices=self.observation_matrix,
                            initial_state_mean=initial_state_mean)
         kf1 = kf1.em(list(measurements), n_iter=2)
-        (smoothed_state_means, smoothed_state_covariances) = kf1.smooth(measurements)
-        return smoothed_state_means
+        return kf1.smooth(measurements)
+
+    def get_prediction(self, measurements):
+        """
+        Compute object position prediction based on current state and Kalman filter's
+        parameters.
+        """
+        prediction = self.make_prediction(measurements)
+        return prediction[0]
 
 
-class PoseCalculator:
+class ConstantAccelerationPoseCalculator(object):
+    """
+    Circular cinematic model based on constant acceleration.
+    """
+
+    def __init__(self):
+        self.radius = 1.6
+        self.theta = 0
+        self.last_pose = Pose()
 
     def get_new_pose(self):
-        global radius
-        global theta
-        theta = theta + 0.01
-        x = radius * math.cos(theta)
-        y = radius * math.sin(theta)
-        z = 0
+        """
+        Compute new pose by incrementing the angle along an imaginary circle.
+        """
 
-        quaternion = quaternion_from_euler(theta, 0, theta)
-        P = Pose(Point(x, y, z), Quaternion(
+        self.theta = self.theta + 0.01
+        x_pos = self.radius * math.cos(self.theta)
+        y_pos = self.radius * math.sin(self.theta)
+        z_pos = 0
+
+        quaternion = quaternion_from_euler(self.theta, 0, self.theta)
+        self.last_pose = Pose(Point(x_pos, y_pos, z_pos), Quaternion(
             quaternion[0], quaternion[1], 0, 0))
 
+        return self.last_pose
+
+    def print_last_pose(self):
+        """
+        Pretty print last pose.
+        """
+
         print "Current pose:"
-        pprint(P)
+        pprint(self.last_pose)
         print "\n"
-        return P
 
 
-class Wrapper:
+class Tracking(object):
+    """
+    Actual tracking implementation.
+    """
+
     def __init__(self):
 
         self.markers = None
         self.update_frecuency = 100.0
 
-        rospy.init_node('test', anonymous=False,
+        rospy.init_node('tracking', anonymous=False,
                         log_level=rospy.INFO, disable_signals=False)
         rospy.on_shutdown(self.cleanup_node)
         self.markers = rviz_tools.RvizMarkers('/map', 'visualization_marker')
 
-        global radius
-        self.kalman_wrapper = KalmanWrapper([radius, 0, 0, 0])
+        self.kalman_wrapper = KalmanWrapper()
         self.deque = collections.deque(maxlen=2)
         self.i = 0
 
     def cleanup_node(self):
+        """
+        Teardown method.
+        """
         print "Shutting down node"
         self.markers.deleteAllMarkers()
 
     def run(self):
+        """
+        Periodic update object position and tracking.
+        """
         while not rospy.is_shutdown():
-            new_pose = PoseCalculator().get_new_pose()
+            new_pose = ConstantAccelerationPoseCalculator().get_new_pose()
             cube_width = 0.6
             # pose, color, cube_width, lifetime
             self.markers.publishCube(new_pose, 'green', cube_width,
@@ -101,5 +142,4 @@ class Wrapper:
             rospy.Rate(self.update_frecuency).sleep()
 
 
-wrapper = Wrapper()
-wrapper.run()
+Tracking().run()
